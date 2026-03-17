bc625d1e7ebc9922316a575fd540a30cbe4ff94d
aelsi2 <eliseevandrey2005@gmail.com>
2026-03-17 14:07:14 +0700

    Make display::update_range a naked asm function

--- src/io/display.rs
+++ src/io/display.rs
@@ -32,40 +32,35 @@ pub fn set_cur2(point: Option<Point>) {
     unsafe { core::ptr::write_volatile(&raw mut MMIO.display_cursors.secondary, value) };
 }
 
-pub fn update_range(fb: &FrameBuf, line_min: isize, line_max: isize) {
-    unsafe {
-        core::arch::asm!(
-            "add r1, r0",
-            "add r1, r0",
-            "add r1, r0",
-            "add r1, r0",
-            "br 1f",
-            "0:",
-            "stb r3, r1",
-            "ldw r0, r5",
-            "stw r4, r5",
-            "add r4, 2",
-            "add r0, 2",
-            "ldw r0, r5",
-            "stw r4, r5",
-            "sub r4, 2",
-            "add r0, 2",
-            "inc r1",
-            "1:",
-            "cmp r1, r2",
-            "ble 0b",
-            in("r0") fb,
-            in("r1") line_min,
-            in("r2") line_max,
-            in("r3") &raw mut MMIO.display.row_index,
-            in("r4") &raw mut MMIO.display.row,
-            lateout("r0") _,
-            lateout("r1") _,
-            lateout("r2") _,
-            lateout("r3") _,
-            lateout("r4") _,
-            lateout("r5") _,
-            options(nostack),
-        );
-    }
+#[unsafe(naked)]
+pub extern "C" fn update_range(fb: &FrameBuf, line_min: isize, line_max: isize) {
+    core::arch::naked_asm!(
+        "push r4",
+        "push r5",
+        "add r1, r0",
+        "add r1, r0",
+        "add r1, r0",
+        "add r1, r0",
+        "ldi r3, {mmio}+4",
+        "ldi r4, {mmio}",
+        "br 1f",
+        "0:",
+        "stb r3, r1",
+        "ldw r0, r5",
+        "stw r4, r5",
+        "add r4, 2",
+        "add r0, 2",
+        "ldw r0, r5",
+        "stw r4, r5",
+        "sub r4, 2",
+        "add r0, 2",
+        "inc r1",
+        "1:",
+        "cmp r1, r2",
+        "ble 0b",
+        "pop r5",
+        "pop r4",
+        "rts",
+        mmio = sym MMIO
+    );
 }
